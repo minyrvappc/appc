7a6f8d4430bcdbd2378241ad353efd05ba60ea4c
minyrvappc <mryoshime@gmail.com>
2026-09-06 23:40:24 +1200

    Update welp.js

--- welp.js
+++ welp.js
@@ -2,3 +2,18 @@ document.querySelector(".left").addEventListener("mouseenter", () => {
   document.querySelectorAll(".left, .right, .middle, #page2, #msg, #white")
     .forEach(el => el.classList.add("animated"));
 });
+
+document.querySelector(".scroll".addEventListener("mouseenter", () => {
+  confetti({
+  particleCount: 300,
+  spread: 90,
+  origin: { x: 1, y: .9 }
+});
+  
+  confetti({
+  particleCount: 300,
+  spread: 90,
+  origin: { x: 0, y: .9 }
+});
+}))
+
